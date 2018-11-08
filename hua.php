e50b9b7fa64a809e7c08c498591a1f080f269c9e
prolhy <44665450+prolhy@users.noreply.github.com>
2018-11-08 18:28:28 +0800

    Add files via upload

---
+++ hua.php
@@ -0,0 +1,25 @@
+<?php
+    int man()
+    {
+        int m n;
+        int a,b,c,t,k;
+            while(scanf("%d%d",&m,&n)==2)
+            {
+                 %k=0;
+                 for(;m>=n;m++)
+                 {
+                     a=m%10;
+                     b=m/10%10;
+                     c=m/100;
+                     t=a*a*a+b*b*b+c*c*c;
+                     if(m==t)
+                     {
+                         if(k==1)printf("")
+                             printd('',)
+                     }
+                 }
+            }
+    }
+
+?>
+
