94e564c57fa5f27b0df2e0248309e8e314945ef8
prolhy <44665450+prolhy@users.noreply.github.com>
2018-11-14 07:54:48 +0800

    Add files via upload

---
+++ 11 13.php
@@ -0,0 +1,11 @@
+public class sum_solution()
+{
+       public int sum_solution(int n)
+    {
+       return (int)(Math.pow(n,2) + n ) >> 1;
+    }
+       public static void main(string[] args)
+    {
+       system.out.printIn(new Sum_Solution(100));
+    }
+}
